7b62543a56a407833ace37b224e489540b2b9328
Mathias Forssen <forssenm@gmail.com>
2013-02-06 22:27:18 +0100

    Added game states

---
+++ Kandidat33MobileGame/src/gamestate/InGameState.java
@@ -0,0 +1,62 @@
+package gamestate;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.input.InputManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
+
+/**
+ * This class handles all the in-game things
+ *
+ * @author forssenm
+ */
+public class InGameState extends AbstractAppState {
+
+    private SimpleApplication app;
+    private Node rootNode;
+    private AssetManager assetManager;
+    private AppStateManager stateManager;
+    private InputManager inputManager;
+    private ViewPort viewPort;
+    private BulletAppState physics;
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = (SimpleApplication) app;
+        this.rootNode = this.app.getRootNode();
+        this.assetManager = this.app.getAssetManager();
+        this.stateManager = this.app.getStateManager();
+        this.inputManager = this.app.getInputManager();
+        this.viewPort = this.app.getViewPort();
+        this.physics = this.stateManager.getState(BulletAppState.class);
+        
+    }
+
+    @Override
+    public void cleanup() {
+        super.cleanup();
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        if (enabled) {
+            //Initiate the things that are needed when the state is active
+            System.out.println("InGameState is now active");
+        } else {
+            //Remove the things not needed when the state is inactive
+            System.out.println("InGameState is now inactive");
+      }
+    }
+ 
+    @Override
+    public void update(float tpf) {
+        
+    }
+}
--- Kandidat33MobileGame/src/main/Main.java
+++ Kandidat33MobileGame/src/main/Main.java
@@ -1,12 +1,8 @@
-package mygame;
+package main;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import menustate.InMainMenuState;
 
 /**
  * test
@@ -21,14 +17,7 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        stateManager.attach(new InMainMenuState());
     }
 
     @Override
