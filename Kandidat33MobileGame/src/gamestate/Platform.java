d913951da648c6ed81d2a2063668768347ef00b2
David Dagson <d_dagson@me.com>
2013-02-12 16:19:18 +0100

    Added a platform class.

---
+++ Kandidat33MobileGame/src/gamestate/Platform.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamestate;
+
+import com.jme3.scene.Geometry;
+
+/**
+ *
+ * @author dagen
+ */
+public class Platform {
+    private Geometry geometry;
+    
+}
