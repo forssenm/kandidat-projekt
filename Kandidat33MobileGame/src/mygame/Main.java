1fb52ffa13ce5a108fc69034aedcd43d491dc614
Jonatan Kilhamn <jonatan.kilhamn@gmail.com>
2013-02-12 11:15:20 +0100

    Revert "Revert "Revert "Jonatan testar git 12:e februari"""
    This reverts commit e05fe9e3888a8451146c7b11a17f55d6f620ae77.

--- Kandidat33MobileGame/src/mygame/Main.java
+++ Kandidat33MobileGame/src/mygame/Main.java
@@ -29,7 +29,7 @@ import java.util.List;
 /**
  * test
  *
- * @author jonatankilhamn som testar git
+ * @author jonatankilhamn
  */
 public class Main extends SimpleApplication implements ActionListener {
 
