4930ef0ccceb694b1fdc3cd89d9e25e958c15e26
David Dagson <d_dagson@me.com>
2013-02-14 17:47:09 +0100

    Definierar en Player klass som fungerar som en struktur för olika spelar relaterade data i syfte att minska antalet lösa instansvariabler i InGameState för att senare inkapsla dem till ett användbart gränssnitt.

--- Kandidat33MobileGame/src/gamestate/Player.java
+++ Kandidat33MobileGame/src/gamestate/Player.java
@@ -1,13 +1,33 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
 package gamestate;
+
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.material.Material;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+
+/* Klassen är till att börja med bara 
+ * en struktur för att knyta saker 
+ * till en spelare. Tanken är att det 
+ * ska utvecklas till att ha ett vettigt 
+ * gränssnitt och eventuellt refaktoreras 
+ * till några java interfaces som ärvs.
+ */
 
 /**
  *
  * @author dagen
  */
 public class Player {
+    public Box playerModel;
+    public Material playerMaterial;
+    public Node playerNode;
+    public CharacterControl playerCharacter;
+    private Player(){}
     
+    public static Player createPlayer(){
+        return new Player();
+    }
 }
