0bcb6b93a931a916fbcedb01cbc879ed31c6c75c
David Dagson <d_dagson@me.com>
2013-02-14 18:23:17 +0100

    Flyttar generatePlayer()-koden till Player.createPlayer().

--- Kandidat33MobileGame/src/gamestate/Player.java
+++ Kandidat33MobileGame/src/gamestate/Player.java
@@ -3,10 +3,15 @@
  */
 package gamestate;
 
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import variables.P;
 
 /* Klassen är till att börja med bara 
  * en struktur för att knyta saker 
@@ -21,13 +26,51 @@ import com.jme3.scene.shape.Box;
  * @author dagen
  */
 public class Player {
-    public Box playerModel;
-    public Material playerMaterial;
-    public Node playerNode;
-    public CharacterControl playerCharacter;
+    public Box model;
+    public Material material;
+    
+    // Hook scenegraph
+    public Node node;
+    public CharacterControl characterControl;
     private Player(){}
     
+    public void addToNode(Node node){
+        node.attachChild(this.node);
+    }
+    public void addToPhysicsSpace(PhysicsSpace physicsSpace){
+        physicsSpace.add(this.characterControl);
+    }
+    
     public static Player createPlayer(){
+        Player player = new Player();
+        player.node = new Node();
+        
+        Geometry playerGeometry = new Geometry("player", player.model);
+        playerGeometry.setMaterial(player.material);       
+        player.node.attachChild(playerGeometry);
+        
+         /**
+         * Create the players CharacterControl object
+         */
+        CapsuleCollisionShape shape = new CapsuleCollisionShape(2f, 2f);
+        player.characterControl = new CharacterControl(shape, 0.05f);
+        player.characterControl.setJumpSpeed(P.jump_speed);
+
+        /**
+         * Position the player
+         */
+        Vector3f vt = new Vector3f(0, 10, 0);
+        player.node.setLocalTranslation(vt);
+
+        player.node.addControl(player.characterControl);
+        Vector3f walkDirection = Vector3f.UNIT_X.multLocal(P.run_speed);
+
+        player.characterControl.setWalkDirection(walkDirection);
         return new Player();
+    }
+    
+    private CapsuleCollisionShape createCapsuleCollisionShape(){
+        return null;
+        
     }
 }
