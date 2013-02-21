e4488783f5330b7bcfed43acf4c031219daa8260
Mathias Forssen <forssenm@gmail.com>
2013-02-21 14:14:44 +0100

    Fixed the randomized platforms, works better now

--- Kandidat33MobileGame/src/gamestate/PlatformController.java
+++ Kandidat33MobileGame/src/gamestate/PlatformController.java
@@ -7,6 +7,7 @@ package gamestate;
 import com.jme3.asset.AssetManager;
 import com.jme3.math.Vector3f;
 import java.util.LinkedList;
+import java.util.Random;
 import variables.P;
 
 /**
@@ -21,16 +22,18 @@ public class PlatformController {
     public PlatformController(PlatformFactory platformFactory){
         platforms = new LinkedList<Platform>();
         Platform platform;
-        Double random;
+        Random random;
+        int randomNumber;
         //Adding the first platform
         platform = platformFactory.createPlatform();
         platform.getRigidBodyControl().setPhysicsLocation(new Vector3f(10f,10f,0));
         platforms.add(platform);
         //Adding all the following platforms
         for(int i = 1; i < P.platformsPerLevel; i++){
-            random = 5*Math.random();
+            random=new Random();
+            randomNumber=(random.nextInt(8)-4);
             platform = platformFactory.createPlatform();
-            platform.getRigidBodyControl().setPhysicsLocation(new Vector3f((P.platformDistance+2*P.platformLength)*i,(float)(platforms.getLast().getRigidBodyControl().getPhysicsLocation().y*random),0));
+            platform.getRigidBodyControl().setPhysicsLocation(new Vector3f((P.platformDistance+2*P.platformLength)*i,(float)(platforms.getLast().getRigidBodyControl().getPhysicsLocation().y+randomNumber),0));
             this.platforms.addLast(platform);
         }
     }
