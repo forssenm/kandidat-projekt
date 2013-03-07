a9c98e58b6483c62db38567a3469a41d7e5f9a64
David Dagson <dagson@student.chalmers.se>
2013-03-07 18:45:11 +0100

    Added javadoc comments for Player.java
    Added javadoc comments for Player.java

--- Kandidat33MobileGame/src/gamestate/Player.java
+++ Kandidat33MobileGame/src/gamestate/Player.java
@@ -4,22 +4,11 @@
 package gamestate;
 
 import com.jme3.asset.AssetManager;
-import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
 import variables.P;
-
-/* Klassen är till att börja med bara 
- * en struktur för att knyta saker 
- * till en spelare. Tanken är att det 
- * ska utvecklas till att ha ett vettigt 
- * gränssnitt och eventuellt refaktoreras 
- * till några java interfaces som ärvs.
- */
 
 /**
  * Class for keeping everything relating to the player in one place.
@@ -32,124 +21,59 @@ public class Player extends Node {
     
     private float playerRunSpeed;
     private float playerJumpSpeed;
+    
     private CapsuleCollisionShape playerShape;
     private CharacterControl playerControl;
+    
     private Node playerModel;
     
-    public Player(AssetManager am) {
+    
+    /**
+     * COnstructs a <code>Player</code> object which is a <Code>Node</code> 
+     * with the players 3D-model attached to it. The <code>Player</code> also 
+     * has a <code>CharacterControl</code> with a <code>CapsuleControl</code> 
+     * attached. 
+     * @param assetManager is used for loading the players assets i.e. 3D-model 
+     * and textures. 
+     */
+    public Player(AssetManager assetManager) {
         playerRunSpeed = P.run_speed;
         playerJumpSpeed = P.jump_speed;
         
         playerShape = new CapsuleCollisionShape(1f,0.5f);
         playerControl = new CharacterControl(playerShape, 0.05f);
-        playerControl.setWalkDirection(Vector3f.UNIT_X.multLocal(playerRunSpeed));
+        playerControl.setWalkDirection(Vector3f.UNIT_X.mult(playerRunSpeed));
         playerControl.setJumpSpeed(playerJumpSpeed);
         this.addControl(playerControl);
         
         this.setLocalTranslation(new Vector3f(0, 15f, 0));
         
         //Sets the model of the player
-        playerModel = (Node)am.loadModel("Models/ghost6anim/ghost6animgroups.j3o");
+        playerModel = (Node)assetManager.loadModel("Models/ghost6anim/ghost6animgroups.j3o");
         this.attachChild(playerModel);
     }
     
-    public void Jump() {
+    /**
+     * Makes the player jump in a manner predefined by 
+     * <code>CharacterControl</code>.
+     */
+    public void jump() {
         playerControl.jump();
     }
     
-    public void increasePlayerSpeed() {
-        // TODO: Implement method
-    }
-    
+    /**
+     * Returns the players <code>CharacterControl</code>
+     * @return the players CharacterControl.
+     */
     public CharacterControl getPlayerControl() {
         return this.playerControl;
     }
     
-    public CapsuleCollisionShape getPlayerShape() {
-        return this.playerShape;
-    }
-    
+    /**
+     * 
+     * @return a reference to the players 3D-model.  
+     */
     public Node getPlayerModel() {
         return this.playerModel;
     }
 }
-
-/*
-public class Player {
-    // Scene Object
-    private Node node;
-    public Spatial spatial;
-    
-    // Physics connection
-    public CharacterControl characterControl;
-    */
-    /**
-     * Generates the player.
-     * @param dataSource An object that generates the spatial to be used to
-     * represent the player.
-     */
-    /*public Player(SceneObjectDataSource dataSource){
-        Node node = new Node();
-        // get the spatial (model, material definition etc)
-        Spatial spatial = dataSource.getSceneObject();
-        
-        // create a CharacterControl, controlling the (physical) behaviour of
-        // the player.
-        CapsuleCollisionShape shape = new CapsuleCollisionShape(1f,0.5f);
-        CharacterControl characterControl = new CharacterControl(shape, 0.05f);
-
-        // set the constant walking direction and jump speed
-        Vector3f walkDirection = Vector3f.UNIT_X.multLocal(P.run_speed);
-        characterControl.setWalkDirection(walkDirection);
-        characterControl.setJumpSpeed(P.jump_speed);
-        
-        // Place the player in the world
-        Vector3f vt = new Vector3f(0, 15f, 0);
-        spatial.setLocalTranslation(vt);
-        
-        
-        spatial.addControl(characterControl);
-        node.attachChild(spatial);
-        
-        this.node = node;
-        this.spatial = spatial;
-        this.characterControl = characterControl;
-    }
-    
-    public void addToNode(Node node){
-        node.attachChild(this.spatial);
-    }
-    
-    public void addToPhysicsSpace(PhysicsSpace physicsSpace){
-        physicsSpace.add(this.characterControl);
-    }
-    
-    public void jump(){
-        this.characterControl.jump();
-    }
-
-    public Node getNode() {
-        return node;
-    }
-
-    public void setNode(Node node) {
-        this.node = node;
-    }
-
-    public Spatial getSpatial() {
-        return spatial;
-    }
-
-    public void setSpatial(Spatial spatial) {
-        this.spatial = spatial;
-    }
-
-    public CharacterControl getCharacterControl() {
-        return characterControl;
-    }
-
-    public void setCharacterControl(CharacterControl characterControl) {
-        this.characterControl = characterControl;
-    }
-}
-*/
