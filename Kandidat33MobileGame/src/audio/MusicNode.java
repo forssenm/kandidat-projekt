02d007ad2f9dea297646631eecb3ae0bef11a0b5
David Dagson <dagson@student.chalmers.se>
2013-03-12 22:43:51 +0100

    Wrote comments for MusicNode.
    Wrote javadoc comments for the MusicNode class.

--- Kandidat33MobileGame/src/audio/MusicNode.java
+++ Kandidat33MobileGame/src/audio/MusicNode.java
@@ -1,21 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package audio;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.audio.AudioNode;
 
 /**
- *
+ * This class is used to play background music. It sets some defaults and 
+ * otherwise works just like {@link AudioNode}. 
  * @author dagen
  */
 public class MusicNode extends AudioNode{
+    /**
+     * Calls super constructor 
+     * {@link AudioNode#AudioNode(com.jme3.asset.AssetManager, java.lang.String, boolean)}
+     * with the input argument and false for the stream argument. The defaults 
+     * are as follows: <br/> 
+     * <code>looping</code> is set to <code>true</code> <br/> 
+     * <code>positional</code> is set to <code>false</code> and <br/>
+     * <code>volume</code> is set to <code>3</code>.
+     * @param assetManager
+     * @param fileName 
+     */
     public MusicNode(AssetManager assetManager,String fileName){
         super(assetManager,fileName, false);
-        setLooping(true);  // activate continuous playing
-        setPositional(false);
+        setLooping(true);
+        setPositional(false); 
         setVolume(3);
     }
 }
