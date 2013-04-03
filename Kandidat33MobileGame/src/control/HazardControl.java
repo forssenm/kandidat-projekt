cdc7f95f157762c4d8ca139fb911cf0421cb231a
Jonatan Kilhamn <jonatan.kilhamn@gmail.com>
2013-04-03 14:26:17 +0200

    Added javadoc for HazardControl

--- Kandidat33MobileGame/src/control/HazardControl.java
+++ Kandidat33MobileGame/src/control/HazardControl.java
@@ -6,10 +6,22 @@ import com.jme3.scene.control.Control;
 import spatial.Player;
 
 /**
+ * An interface for hazards that do something when they touch the player. An
+ * object that implements this is a Control and thus attached to a
+ * <code>Spatial</code>.
  *
  * @author dagen
  */
 public interface HazardControl extends Control {
 
+    /**
+     * Called when this hazard collides with the player.
+     * When e.g. a
+     * <code>PhysicsCollisionListener</code> finds that a hazard-player-collision
+     * has happened, this method is called and containts the right reaction –
+     * deal damage to or move the player etc.
+     *
+     * @param player
+     */
     public void collideWithPlayer(Spatial player);
 }
