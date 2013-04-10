462c95deb2369c75cb17833aa8a02c75d6412b3c
Emil <emilbzm@gmail.com>
2013-04-10 06:37:54 +0200

    New wizard model
    Added cloth based model for wizard and a particle effect.
    No animation / texture yet.
    Removed old ghost model files.

--- Kandidat33MobileGame/src/spatial/hazard/Wizard.java
+++ Kandidat33MobileGame/src/spatial/hazard/Wizard.java
@@ -1,11 +1,16 @@
 package spatial.hazard;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import control.HazardControl;
@@ -26,17 +31,31 @@ public class Wizard extends Hazard {
         this.addControl(createControl());
     }
 
+    
     @Override
     protected Spatial createModel(AssetManager assetManager) {
-        Box model =
+        /*Box model =
                 new Box(Vector3f.ZERO, 0.2f, 2f, 0.2f);
         Geometry geometry = new Geometry("", model);
 
         Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        material.setColor("Color", ColorRGBA.Black);
+        material.setColor("Color", ColorRGBA.Yellow);
         geometry.setMaterial(material);
         geometry.setShadowMode(RenderQueue.ShadowMode.Cast);
+        
         return geometry;
+        * */
+        // Node playerModel = (Node) assetManager.loadModel("Models/ghost6anim/clothball.j3o");
+        Node playerModel = (Node) assetManager.loadModel("Models/wizard/wizardcloth005.j3o");
+
+        playerModel.setLocalRotation((new Quaternion()).fromAngles(0f,90*FastMath.DEG_TO_RAD,0f));
+        playerModel.scale(1.5f);
+        ParticleEmitter sparkle = getWandParticleEmitter();
+        playerModel.attachChild(sparkle);
+       //denna positionering (z-led) ser konstig ut men har att göra med att modellen är roterad 90 grader. Bättre vore att ha en modell som inte behöver roteras. Jobbar på det /130410
+        sparkle.move(0f, 2.0f, -2.0f); 
+       
+         return playerModel;
     }
 
     @Override
@@ -45,4 +64,27 @@ public class Wizard extends Hazard {
         return wizardControl;
     }
     
+    
+    private ParticleEmitter getWandParticleEmitter () {
+             ParticleEmitter fire = 
+            new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 10);
+    Material mat_red = new Material(assetManager, 
+            "Common/MatDefs/Misc/Particle.j3md");
+    mat_red.setTexture("Texture", assetManager.loadTexture(
+            "Effects/Explosion/flame.png"));
+   // mat_red.getAdditionalRenderState().setBlendMode(BlendMode.Alpha); för att kunna göra svarta partiklar
+    fire.setMaterial(mat_red);
+    fire.setImagesX(2); 
+    fire.setImagesY(2); // 2x2 texture animation
+    fire.setStartColor( new ColorRGBA(.10f, 0.40f, 0.90f, 1f));   // bright cyan
+    fire.setEndColor(new ColorRGBA(0f, 0.1f, 0.25f, 0.5f)); // dark blue
+    fire.getParticleInfluencer().setInitialVelocity(new Vector3f(0, -5, 0));
+    fire.setStartSize(1.5f);
+    fire.setEndSize(0.5f);
+    fire.setGravity(0, 0f, 0);
+    fire.setLowLife(0.2f);
+    fire.setHighLife(0.5f);
+    fire.getParticleInfluencer().setVelocityVariation(0.3f);
+    return fire;
+    }
 }
