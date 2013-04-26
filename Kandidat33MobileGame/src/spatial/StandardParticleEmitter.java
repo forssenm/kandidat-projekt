42be7a0dc38df05c4bef3e39c2ae2e4b1bf1d53c
Emil <emilbzm@gmail.com>
2013-04-26 03:01:05 +0200

    StandardparticleEmitter Forcefield
    Added a new type of standard particle emitter. force field.

--- Kandidat33MobileGame/src/spatial/StandardParticleEmitter.java
+++ Kandidat33MobileGame/src/spatial/StandardParticleEmitter.java
@@ -4,6 +4,8 @@ import com.jme3.asset.AssetManager;
 import com.jme3.effect.ParticleEmitter;
 import com.jme3.effect.ParticleMesh;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 /**
  *
  * @author natwei
@@ -13,6 +15,7 @@ public class StandardParticleEmitter extends ParticleEmitter {
     //private static Material materialForParticles;
     
     public StandardParticleEmitter () {}
+    
     public static ParticleEmitter make (AssetManager assetManager) {
         ParticleEmitter fire = 
             new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 30);
@@ -26,5 +29,24 @@ public class StandardParticleEmitter extends ParticleEmitter {
             fire.setImagesX(2); 
             fire.setImagesY(2); // 2x2 texture animation
         return fire;
+    }
+    
+    public static ParticleEmitter forcefield (AssetManager assetManager) {
+        ParticleEmitter glow = StandardParticleEmitter.make(assetManager);
+         glow.getMaterial().setTexture("Texture", assetManager.loadTexture(
+            "Textures/Explosion/shockwave.png"));
+        glow.setImagesX(1);
+        glow.setImagesY(1);
+        glow.setNumParticles(1);
+        glow.setStartColor(ColorRGBA.Yellow);
+        glow.setEndColor(ColorRGBA.White);
+        glow.getParticleInfluencer().setInitialVelocity(Vector3f.ZERO);
+        glow.setStartSize(0.1f);
+        glow.setEndSize(3.51f);
+        glow.setGravity(0, 0, 0);
+        glow.setLowLife(2f);
+        glow.setHighLife(2f);
+        glow.getParticleInfluencer().setVelocityVariation(0.3f);
+        return glow;
     }
 }
