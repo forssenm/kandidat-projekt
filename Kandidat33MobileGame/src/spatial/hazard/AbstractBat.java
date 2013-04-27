f56cdd125289ef86524845944b38a8b713818578
Emil <emilbzm@gmail.com>
2013-04-27 05:42:30 +0200

    Fixed bat rotation and testing random colors
    Set the rotation for the bat so it faces the player
    Testing random material colors to the bat. The colors are not intended
    to be permanent, but it's good to see how this works, and also they need
    better visibility with the current lighting settings.
    Randomizing between different brown / grey / black / other colors can be
    nice for the visual variety.

--- Kandidat33MobileGame/src/spatial/hazard/AbstractBat.java
+++ Kandidat33MobileGame/src/spatial/hazard/AbstractBat.java
@@ -5,14 +5,17 @@ import com.jme3.animation.AnimControl;
 import com.jme3.animation.AnimEventListener;
 import com.jme3.animation.LoopMode;
 import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import java.util.Random;
 import spatial.PlayerInteractor;
 
 /**
  * An abstract AbstractFireball. Any class extending this one will make a PlayerInteractor
  * that looks like a fireball.
- *
+ * 
  * @author jonatankilhamn
  */
 public abstract class AbstractBat extends PlayerInteractor implements AnimEventListener {
@@ -24,7 +27,9 @@ public abstract class AbstractBat extends PlayerInteractor implements AnimEventL
     @Override
     protected Spatial createModel(AssetManager assetManager) {
         Node batModel = (Node) assetManager.loadModel ("Models/bat/bat02-002mirror006anim2fix.j3o");
-        batModel.rotate(1.0f, 0.7f, 0);  //bat special 
+        
+        batModel.setMaterial(getRandomColorMaterial(assetManager));
+        batModel.rotate (+1.6f,+1.4f,0); //flying towards player, slightly tilted up
         control = batModel.getChild("Sphere").getControl(AnimControl.class);
         channel = control.createChannel();
         
@@ -41,5 +46,16 @@ public abstract class AbstractBat extends PlayerInteractor implements AnimEventL
     }
     //animation function that must be implemented even if unused
     public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
+    }
+    
+    public Material getRandomColorMaterial (AssetManager assetManager) {
+         Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+                  
+         Random r = new Random();
+         int i = r.nextInt(5);
+         mat.setBoolean("UseMaterialColors",true);
+         ColorRGBA[] c = {ColorRGBA.Cyan, ColorRGBA.Red, ColorRGBA.White, ColorRGBA.Orange, ColorRGBA.Yellow};
+         mat.setColor("Diffuse", c[i]);
+         return mat;
     }
 }
