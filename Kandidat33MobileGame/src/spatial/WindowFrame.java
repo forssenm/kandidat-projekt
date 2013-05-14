28cfb15042e3822e5bc5d78e324000c1513760e4
Nina <ninja.malm@gmail.com>
2013-05-14 18:26:05 +0200

    Added light textures and models for windows.

--- Kandidat33MobileGame/src/spatial/WindowFrame.java
+++ Kandidat33MobileGame/src/spatial/WindowFrame.java
@@ -21,15 +21,29 @@ import variables.P;
 public class WindowFrame extends Node {
     
     public enum Design {
-        FLOWERS ("Models/window/flat-window-2.j3o"),
-        BIRD ("Models/window/flat-window-2b.j3o");
+        FLOWERS ("Models/window/flat-window-2.j3o",
+                "Models/window/Light/flat-window-2.j3o",
+                "Models/window/Light/texture1_light.png",
+                "Models/window/Light/texture1_light_colors.png"),
+        BIRD ("Models/window/flat-window-2b.j3o",
+                "Models/window/Light/flat-window-2b.j3o",
+                "Models/window/Light/texture2_light.png",
+                "Models/window/Light/texture2_light_colors.png");
 
         public final String modelSrc;
+        public final String lightModelSrc;
+        public final String wallLightSrc;
+        public final String lightColorsSrc;
 
-        Design(String modelSrc) {
+        Design(String modelSrc, String lightModelSrc, String wallLightSrc, String lightColorsSrc) {
             this.modelSrc = modelSrc;
+            this.lightModelSrc = lightModelSrc;
+            this.wallLightSrc = wallLightSrc;
+            this.lightColorsSrc = lightColorsSrc;
         }
     }
+    
+    private Design design;
 
     /**
      * This constructor creates a
@@ -41,8 +55,15 @@ public class WindowFrame extends Node {
      */
     public WindowFrame(AssetManager assetManager, Vector3f position, Design design ) {
         super("WindowFrame");
+        this.design = design;
         
-        Node window = (Node)assetManager.loadModel(design.modelSrc);
+        Node window = null;
+        if (EffectSettings.light == EffectSettings.Light.TEXTURES) {
+            window = (Node)assetManager.loadModel(design.lightModelSrc);
+            
+        } else {
+            window = (Node)assetManager.loadModel(design.modelSrc);
+        }
         window.scale(4.5f);
         window.rotate(90*FastMath.DEG_TO_RAD, 0f, 0f);
         
@@ -62,7 +83,7 @@ public class WindowFrame extends Node {
         Geometry wall = new Geometry("wallLighting", wallLight);
         wall.setLocalTranslation(0f, -8f/3f, 6.6f);
         Material wallMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        wallMaterial.setTexture("ColorMap", assetManager.loadTexture("Models/window/Light/texture1_light.png"));
+        wallMaterial.setTexture("ColorMap", assetManager.loadTexture(design.wallLightSrc));
         wallMaterial.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha); // activate transparency
         wall.setMaterial(wallMaterial);
         wall.setQueueBucket(RenderQueue.Bucket.Transparent);
